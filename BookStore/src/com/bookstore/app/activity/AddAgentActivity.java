c1621f5d891f3fca79fbc012da9b6c6054ac79c3
Sajedul karim <mesukcse08@gmail.com>
2015-04-17 16:59:45 +0600

    Mahbub
    Add agent initview

--- BookStore/src/com/bookstore/app/activity/AddAgentActivity.java
+++ BookStore/src/com/bookstore/app/activity/AddAgentActivity.java
@@ -16,6 +16,11 @@ public class AddAgentActivity extends BookStoreActionBarBase{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_agent);
+		initalization();
+	}
+	private void initalization() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
